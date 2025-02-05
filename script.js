7a95ad9372956bda7dc0c4dd2321288634f3d77f
Gunaseelan7704 <guna57275@gmail.com>
2025-02-05 15:47:58 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function calculate(){
+    const totalamnt = document.getElementById("total-amnt");
+    const principalAmnt = document.getElementById("principal");
+    const rateInp = document.getElementById("rate");
+    const yearsInp = document.getElementById("years"); 
+    
+    let principal = Number(principalAmnt.value);
+    let rate = Number(rateInp.value / 100) ;
+    let years = Number(yearsInp.value);
+
+    const interest = (principal * 12*years * rate) / 100 * 100;
+    console.log(interest);
+    const result = principal + interest;
+
+    totalamnt.textContent = result.toLocaleString(undefined,{style : "currency", currency: "INR"});
+}
